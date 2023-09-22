998db013cebc291dad6495a16d1f352efe3b1885
Gurpal Singh <gillgurpal699@gmail.com>
2023-09-22 21:28:28 +0530

    update home screen

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -25,3 +25,29 @@ enum Status {
   pending,
   sent,
 }
+
+@collection
+class Workout {
+  Id id = Isar.autoIncrement;
+
+  String? name;
+
+  List<Exercise>? exercises;
+}
+
+class Exercise {
+  /// Name of the Exercise. Ex: 10 Pushups
+  String? name;
+
+  /// Time at which the exercise will start, required for first exercise and then optional.
+  DateTime? startTime;
+
+  /// Not Required for any exercise but can be added. For Example: 30s jumping jacks
+  DateTime? endTime;
+
+  /// How long the break should be after this exercise. Must be in seconds and a whole number
+  int? breakDuration;
+
+  /// The total number of reps. A whole number
+  int? repetition;
+}
--- lib/screens/main.dart
+++ lib/screens/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:dot_navigation_bar/dot_navigation_bar.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,12 +10,19 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int currentIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       appBar: AppBar(
         title: Text(
-          "Workouts",
+          "JustFit",
           style: GoogleFonts.hankenGrotesk(fontSize: 22, fontWeight: FontWeight.bold),
         ),
         actions: [
@@ -25,7 +33,7 @@ class _HomeScreenState extends State<HomeScreen> {
               child: IconButton(
                 onPressed: () {},
                 icon: const Icon(
-                  Icons.add,
+                  Icons.settings,
                   size: 25,
                 ),
                 style: ButtonStyle(
@@ -35,6 +43,126 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ],
+      ),
+      bottomNavigationBar: DotNavigationBar(
+        backgroundColor: Color(0xff212121),
+        currentIndex: currentIndex, enablePaddingAnimation: false, splashColor: Colors.transparent,
+        onTap: (p0) {
+          setState(() {
+            currentIndex = p0;
+          });
+        },
+        dotIndicatorColor: Colors.black,
+        // enableFloatingNavBar: false
+        items: [
+          DotNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                size: 30,
+              ),
+              selectedColor: Colors.white),
+
+          /// Likes
+          DotNavigationBarItem(
+            icon: Image.asset("assets/risky.png"),
+          ),
+
+          /// Search
+          DotNavigationBarItem(
+              icon: Icon(
+                Icons.add,
+                size: 30,
+              ),
+              selectedColor: Colors.white),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 85,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color(0xff495057),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "69",
+                          style: GoogleFonts.hankenGrotesk(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Workouts",
+                          style: GoogleFonts.hankenGrotesk(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 50,
+                ),
+                Container(
+                  constraints: BoxConstraints(
+                    minWidth: 85,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    color: const Color(0xff495057),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          "420",
+                          style: GoogleFonts.hankenGrotesk(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "Minutes",
+                          style: GoogleFonts.hankenGrotesk(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
