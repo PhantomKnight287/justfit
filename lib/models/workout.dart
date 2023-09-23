8eee3cdd71e820fb014fd542b4ed8a4ddf100dfd
Gurpal Singh <gillgurpal699@gmail.com>
2023-09-23 22:16:15 +0530

    add emojis in workout class

--- lib/models/workout.dart
+++ lib/models/workout.dart
@@ -1,38 +1,15 @@
 import 'package:isar/isar.dart';
 
 @collection
-class Email {
-  Id id = Isar.autoIncrement; // you can also use id = null to auto increment
-
-  @Index(type: IndexType.value)
-  String? title;
-
-  List<Recipient>? recipients;
-
-  @enumerated
-  Status status = Status.pending;
-}
-
-@embedded
-class Recipient {
-  String? name;
-
-  String? address;
-}
-
-enum Status {
-  draft,
-  pending,
-  sent,
-}
-
-@collection
 class Workout {
   Id id = Isar.autoIncrement;
 
   String? name;
 
   List<Exercise>? exercises;
+
+  /// List to paths pointing to actual bundled images
+  List<String>? emojis;
 }
 
 class Exercise {
