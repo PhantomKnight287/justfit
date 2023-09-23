931fa2164b45d7b7b3c59201e3b895f03f4ab68b
Gurpal Singh <gillgurpal699@gmail.com>
2023-09-23 22:16:00 +0530

    add new constants

--- lib/constants/main.dart
+++ lib/constants/main.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: constant_identifier_names
+
 import 'package:flutter/material.dart';
 
 const DARK_BG = Color(0xff101113);
 const BRAND_HEX = 0xfff0e100;
 const BRAND_COLOR = Color(BRAND_HEX);
 const LIGHT_COLOR = Color(0xffE9ECEF);
+
+const EMOJIS = ["abs", "arms", "legs"];
